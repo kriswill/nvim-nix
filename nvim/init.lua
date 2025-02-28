3e9151939cc0250c4c24e24468901fbeda94e149
Kris Williams <115474+kriswill@users.noreply.github.com>
2025-02-27 17:59:48 -0800

    local leader

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,8 @@ local opt = vim.o
 local g = vim.g
 
 -- <leader> key. Defaults to `\`. Some people prefer space.
--- g.mapleader = ' '
--- g.maplocalleader = ' '
+g.mapleader = ' '
+g.maplocalleader = ' '
 
 opt.compatible = false
 
@@ -94,7 +94,7 @@ vim.diagnostic.config {
 
 g.editorconfig = true
 
-vim.opt.colorcolumn = '100'
+-- vim.opt.colorcolumn = '100'
 
 -- Native plugins
 cmd.filetype('plugin', 'indent', 'on')
